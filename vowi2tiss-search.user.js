22fb10c1802a7607c4e5683195aa1b6624b2a061
Gittenburg <47354597+Gittenburg@users.noreply.github.com>
2019-02-20 16:37:44 +0100

    vowi2tiss-search.user.js: improve UX, pass type

--- vowi2tiss-search.user.js
+++ vowi2tiss-search.user.js
@@ -3,18 +3,20 @@
 // @namespace https://vowi.fsinf.at/
 // @match https://vowi.fsinf.at/wiki/*
 // @match https://tiss.tuwien.ac.at/course/courseList.xhtml*
+// @description Does not work with Greasemonkey because of https://github.com/greasemonkey/greasemonkey/issues/2700
 // ==/UserScript==
 
 if (location.host == 'vowi.fsinf.at') {
 	if (document.getElementById('lva-daten') != null) {
 		var content = document.getElementById('mw-content-text');
 		var a = document.createElement('a');
+		a.setAttribute('target', '_blank');
 		a.innerHTML = 'TISS Suche';
 		content.insertBefore(a, content.firstChild);
-		var title = document.getElementById('firstHeading').innerHTML;
-		a.href = 'https://tiss.tuwien.ac.at/course/courseList.xhtml?title=' + encodeURIComponent(
-			title.substring(title.indexOf(':') + 1, title.indexOf('(') - 4)
-		)
+		var heading = document.getElementById('firstHeading').innerHTML;
+		var title = encodeURIComponent(heading.substring(heading.indexOf(':') + 1, heading.indexOf('(') - 4));
+		var type = encodeURIComponent(heading.substr(heading.indexOf('(') - 3, 2));
+		a.href = 'https://tiss.tuwien.ac.at/course/courseList.xhtml?title=' + title + '&type=' + type;
 	}
 } else if (location.host == 'tiss.tuwien.ac.at') {
 	var params = new URL(location).searchParams;
@@ -22,10 +24,17 @@ if (location.host == 'vowi.fsinf.at') {
 		jsf.ajax.addOnEvent(function (data) {
 			if (data.status == 'success') {
 				document.getElementById('courseList:courseTitle').value = params.get('title')
+				document.getElementById('courseList:courseType').value = params.get('type')
 				document.getElementById('courseList:semFrom').value = '2015W'
 				document.getElementById('courseList:cSearchBtn').click()
 			}
 		})
 		document.getElementById('courseList:quickSearchPanel').children[0].lastElementChild.click()
+	} else {
+		var titleInput = document.getElementById('courseList:courseTitle');
+		if (titleInput) {
+			document.getElementById('courseList:courseLecturer').focus()
+			window.find(titleInput.value);
+		}
 	}
 }
