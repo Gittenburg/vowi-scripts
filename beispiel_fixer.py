1559a7513b85b885acb78735a24037294ccc3b76
Gittenburg <47354597+Gittenburg@users.noreply.github.com>
2019-03-01 15:53:13 +0100

    update beispiel_fixer.py

--- beispiel_fixer.py
+++ beispiel_fixer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import argparse
+import re
 
 import mwapi
 import mwbot
@@ -17,7 +18,7 @@ def handle_index(site, index):
 			continue
 
 		code = mwparserfromhell.parse(orig)
-		templates = code.filter_templates(matches=lambda x: x.name.matches('Beispiel'))
+		templates = code.filter_templates(matches=lambda x: x.name.matches('Beispiel') or x.name.matches('Bsp'))
 		if len(templates) > 0:
 			template = templates[0]
 		else:
@@ -25,6 +26,7 @@ def handle_index(site, index):
 			code.insert(0, template)
 
 		# legacy format handling
+		template.name = 'Beispiel'
 		if template.has('status'):
 			fields = template.get('status').value.split(';')
 			if len(fields) == 1:
@@ -38,18 +40,15 @@ def handle_index(site, index):
 			else:
 				print('unknown status: {}'.format(k))
 
-		if template.has('hilfreiches'):
-			template.get('hilfreiches').name = 'bausteine'
-
 		if template.has('gekommen'):
 			template.get('gekommen').value = str(template.get('gekommen').value).replace('/', ';')
 
 		if template.has('1'):
 			if str(template.get('1').value).strip() == 'teils':
-				template.add('status', 'teils')
+				template.add('teils', '')
 				template.remove('1')
 			elif str(template.get('1').value).strip() == 'falsch':
-				template.add('status', 'falsch')
+				template.add('falsch', '')
 				template.remove('1')
 
 		if not template.has('1'):
@@ -67,17 +66,9 @@ def handle_index(site, index):
 		hilfreiches = code.get_sections(matches='Hilfreiches', include_headings=False)
 		if hilfreiches:
 			for bstn in hilfreiches[0].filter_templates():
-				if not template.has('bausteine'):
-					template.add('bausteine', '', preserve_spacing=False)
+				bstn.name = re.sub('^([Vv]orlage|[Tt]emplate):', '', str(bstn.name))
 				if not bstn.name.startswith('Baustein'):
 					bstn.name = 'Baustein:' + str(bstn.name)
-				if not str(bstn) in str(template.get('bausteine').value):
-					template.get('bausteine').value.append('\n' + str(bstn))
-				code.remove(bstn)
-			if str(hilfreiches[0]).strip() == '':
-					code.remove(code.get_sections(matches='Hilfreiches')[0])
-		if template.has('bausteine') and not template.get('bausteine').value.endswith('\n'):
-			template.get('bausteine').value.append('\n')
 
 		mwbot.save(site, page['title'], orig, str(code), 'beispiel_fixer.py', strip_consec_nl=True)
 
