85af4ae64c073956f0648e37b3e45b6ec73bcd6d
Gittenburg <47354597+Gittenburg@users.noreply.github.com>
2019-03-07 06:42:44 +0100

    fix old_mat_counter.py

--- old_mat_counter.py
+++ old_mat_counter.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
+import argparse
+
 import mwapi
 import mwbot
 import vowi
 
-site = mwbot.getsite()
-
-old_lvas = []
-
-for title in site.get('askargs', conditions='Category:LVAs|Ist veraltet::1', parameters='limit=9999')['query']['results']:
-	old_lvas.append(title)
-
 def count_old_resources():
-	old_resources = 0
+	old_lvas = []
 
-	for page in site.query('pages', generator='categorymembers', gcmtitle='Category:Materialien',
+	for title in site.post('askargs', conditions='Category:LVAs|Ist veraltet::1', parameters='limit=9999')['query']['results']:
+		old_lvas.append(title)
+		old_resources = 0
+
+	for page in site.results(generator='categorymembers', gcmtitle='Category:Materialien',
 			gcmnamespace=mwapi.NS_FILE, gcmlimit='max', prop='links', pllimit='max'):
 		
 		if 'links' in page:
@@ -25,13 +24,20 @@ def count_old_resources():
 		else:
 			print('no links:', page['title'])
 
-	for page in site.query('categorymembers', list='categorymembers', cmtitle='Category:Materialien',
+	for page in site.results(list='categorymembers', cmtitle='Category:Materialien',
 			cmnamespace=mwapi.join(vowi.UNI_NAMESPACES), cmlimit='max'):
 		if page['title'].split('/')[0] in old_lvas:
 			old_resources += 1
 	return old_resources
 
 if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--dry', action='store_true')
+	args = parser.parse_args()
+
+	site = mwbot.getsite()
+
 	count = count_old_resources()
 	print(count)
-	mwbot.edit(site, 'VoWi:Anzahl veralteter Materialien', '{}<noinclude>__NOINDEX__ (generiert von old_mat_counter.py)</noinclude>'.format(count), 'update')
+	if not args.dry:
+		mwbot.edit(site, 'VoWi:Anzahl veralteter Materialien', '{}<noinclude>__NOINDEX__ (generiert von old_mat_counter.py)</noinclude>'.format(count), 'update')
