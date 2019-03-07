237d7aa9b43df03b5b245f297f701414191285b4
Gittenburg <47354597+Gittenburg@users.noreply.github.com>
2019-03-07 06:34:25 +0100

    update indextable.py to beispieltable.py

--- tools/beispieltable.py
+++ tools/beispieltable.py
@@ -4,12 +4,15 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-min', nargs='?', type=int, default=1)
 parser.add_argument('max', type=int)
-parser.add_argument('linkprefix', nargs='?', default='')
+parser.add_argument('linkprefix', nargs='?', default='Beispiel ')
 args = parser.parse_args()
 
-print('{| class=indextable')
+print('{| class=beispiel-table')
 for i in range(args.min, args.max+1):
 	print('| [[/{}{}|{}]]'.format(args.linkprefix, i, i))
 	if i != args.max and i % 10 == 0:
 		print('|-')
+	if i != args.max and i % 100 == 0:
+		print('|}')
+		print('{| class=beispiel-table')
 print('|}')
