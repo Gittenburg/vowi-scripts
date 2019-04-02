b2d0b48c367a574c6130ba8452a372b8a6ea15b5
Gittenburg <47354597+Gittenburg@users.noreply.github.com>
2019-04-02 18:11:59 +0200

    add MissingFunctions extension

---
+++ MissingFunctions/MissingFunctions.i18n.php
@@ -0,0 +1,6 @@
+<?php
+$magicWords = [];
+$magicWords['en'] = [
+	'maptemplate' => [ 0, 'maptemplate' ],
+	'frametitle' => [ 0, 'frametitle' ]
+];
---
+++ MissingFunctions/MissingFunctionsHooks.php
@@ -0,0 +1,41 @@
+<?php
+class MissingFunctionsHooks {
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setFunctionHook( 'maptemplate', [ self::class, 'renderMapTemplate' ]);
+		$parser->setFunctionHook( 'frametitle', [ self::class, 'renderFrameTitle' ], SFH_OBJECT_ARGS);
+	}
+
+	/* Parser function to split a string and pass the parts as parameters to a template. */
+	public static function renderMapTemplate( Parser $parser, $text, $template, $insep='', $outsep='', $userparam='') {
+		if (empty($text))
+			return '';
+
+		$insep  = str_replace('\n', "\n", $parser->mStripState->unstripNoWiki($insep));
+		$outsep = str_replace('\n', "\n", $parser->mStripState->unstripNoWiki($outsep));
+		$output = '';
+
+		if (empty($insep))
+			$insep = ' ';
+
+		$values = explode($insep, $text);
+
+		foreach ($values as $idx=>$val){
+			$output .= $parser->replaceVariables("{{{$template}|1=$val|userparam=$userparam}}");
+			if ($idx != count($values)-1)
+				$output .= $outsep;
+		}
+		return [$output, []];
+	}
+
+	/* Parser function to allow templates to access their own title or the title of a parent. */
+	public static function renderFrameTitle( Parser $parser, $frame, $args) {
+		$level = intval($args[0]); // 0 is own title
+		while ($level-- > 0){
+			if ($frame->parent)
+				$frame = $frame->parent;
+			else
+				return "";
+		}
+		return $frame->getTitle();
+	}
+}
