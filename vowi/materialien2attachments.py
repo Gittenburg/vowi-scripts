87492d3bdb176a98901ff8aa6e6b24d9864198f9
Gittenburg <47354597+Gittenburg@users.noreply.github.com>
2019-05-07 21:04:49 +0200

    materialien2attachments.py add multi-threading

--- vowi/materialien2attachments.py
+++ vowi/materialien2attachments.py
@@ -1,38 +1,53 @@
 #!/usr/bin/env python3
+from concurrent.futures import ThreadPoolExecutor as PoolExecutor
 import re
 
 import mwbot
 
-parser = mwbot.get_argparser()
-args = parser.parse_args()
-site = mwbot.getsite('materialien2attachments.py', args)
+THREADS = 10
+
+def convert_redirect(r):
+	before = r['revisions'][0]['*']
+	redir = mwbot.parse_redirect(before)
+	if redir and redir[0].startswith('http'):
+		text = '{{#exturl:%s}}' % redir[0].strip()
+		if redir[1] and redir[1] != r['title'].split('/')[-1].replace('_', ' '):
+			text += '\n[[Hat Linkbeschreibung::%s]]' % redir[1]
+		mwbot.save(site, r['title'], before, text, 'verwende #exturl Parserfunktion')
+
 
 def convert_redirects():
-	for r in site.results(generator='querypage', gqppage='BrokenRedirects', gqplimit='max', prop='revisions', rvprop='content'):
-		before = r['revisions'][0]['*']
-		redir = mwbot.parse_redirect(before)
-		if redir and redir[0].startswith('http'):
-			text = '{{#exturl:%s}}' % redir[0].strip()
-			if redir[1] and redir[1] != r['title'].split('/')[-1].replace('_', ' '):
-				text += '\n[[Hat Linkbeschreibung::%s]]' % redir[1]
-			mwbot.save(site, r['title'], before, text, 'verwende #exturl Parserfunktion')
+	results = list(site.results(generator='querypage', gqppage='BrokenRedirects', gqplimit='max', prop='revisions', rvprop='content'))
+
+	with PoolExecutor(max_workers=THREADS) as executor:
+		for _ in executor.map(convert_redirect, results):
+			pass
+
+def convert_file(r):
+	text = r['revisions'][0]['*']
+	parts = text.split("<!-- Don't edit below this line! -->")
+	after = parts[0]
+	if len(parts) == 2:
+		for line in parts[1].splitlines():
+			match = re.match('\[\[([^]]+)\]\]', line)
+			if match:
+				title = match.group(1).strip().replace('_', ' ')
+				if not title.startswith('Kategorie'):
+					after += '{{#attach:%s}}\n' % title
+	else:
+		print('cannot handle', r['title'])
+
+	mwbot.save(site, r['title'], text, after, 'verwende #attach Parserfunktion')
 
 def convert_files():
-	for r in site.results(generator='categorymembers', gcmtitle='Category:Materialien', gcmtype='file', prop='revisions', rvprop='content', gcmlimit='max'):
-		text = r['revisions'][0]['*']
-		parts = text.split("<!-- Don't edit below this line! -->")
-		after = parts[0]
-		if len(parts) == 2:
-			for line in parts[1].splitlines():
-				match = re.match('\[\[([^]]+)\]\]', line)
-				if match:
-					title = match.group(1).strip().replace('_', ' ')
-					if not title.startswith('Kategorie'):
-						after += '{{#attach:%s}}\n' % title
-		else:
-			print('cannot handle', r['title'])
+	results = list(site.results(generator='categorymembers', gcmtitle='Category:Materialien', gcmtype='file', prop='revisions', rvprop='content', gcmlimit='max'))
+	with PoolExecutor(max_workers=THREADS) as executor:
+		for _ in executor.map(convert_file, results):
+			pass
 
-		mwbot.save(site, r['title'], text, after, 'verwende #attach Parserfunktion')
-
-convert_redirects()
-convert_files()
+if __name__ == '__main__':
+	parser = mwbot.get_argparser()
+	args = parser.parse_args()
+	site = mwbot.getsite('materialien2attachments.py', args)
+	convert_redirects()
+	convert_files()
