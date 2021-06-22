5352d39b4ebc019219c0403f7eef9bdf4dbe92b0
Gittenburg <gittenburg@fsinf.at>
2021-06-22 23:31:28 +0200

    somewhat fix general/subpage_mover.py

--- general/subpage_mover.py
+++ general/subpage_mover.py
@@ -4,17 +4,17 @@ import os.path
 
 import mwbot
 
-parser = argparse.ArgumentParser()
+parser = mwbot.get_argparser()
 parser.add_argument('src')
 parser.add_argument('dest')
 parser.add_argument('reason')
 parser.add_argument('--noredir', action='store_true')
 args = parser.parse_args()
 
-site = mwbot.getsite()
+site = mwbot.getsite('subpage_mover.py', args)
 site.require_rights('move')
 
-src_ns = next(site.query('pages', prop='info', titles=args.src))['ns']
+src_ns = next(site.results(list='pages', prop='info', titles=args.src))['ns']
 
 if args.dest.startswith('.'):
 	destname = os.path.abspath('/' + args.src + '/' + args.dest)[1:]
@@ -28,7 +28,7 @@ moves = []
 
 srcname = args.src.split(':', 2)[1]
 
-for page in site.query('allpages', list='allpages', apprefix=args.src.split(':', 2)[1]+'/', apnamespace=src_ns, aplimit='max'):
+for page in site.results(list='allpages', apprefix=args.src.split(':', 2)[1]+'/', apnamespace=src_ns, aplimit='max'):
 	moves.append((page['title'], destname + page['title'].split(':',2)[1][len(srcname):]))
 
 moves.append((args.src, destname))
@@ -37,7 +37,7 @@ for idx, (src, dest) in enumerate(reversed(moves), 1):
 	print('move {}/{}:'.format(idx, len(moves)))
 	print('from:', src)
 	print('to:  ', dest)
-	dest = next(site.query('pages', titles=dest))
+	dest = next(site.results(list='pages', titles=dest))
 	if not 'missing' in dest:
 		if input('destination already exists, skip?') == '':
 			del moves[-idx]
